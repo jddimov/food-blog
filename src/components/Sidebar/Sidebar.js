0431ef34bb1793ca76b50ee7ad821071b4bd5641
jddimov <jddimov@abv.bg>
2022-01-26 17:33:28 +0200

    fix css, containers, sidebar menu

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,9 +1,9 @@
 import React, { useRef } from 'react'
-import { SidebarContainer, Icon, CloseIcon, SidebarWrapper, SidebarMenu, SidebarLink } from './SidebarElement'
-import { Link } from 'react-router-dom'
+import { SidebarContainer, Icon, CloseIcon, SidebarMenu, SidebarLink, Link } from './SidebarElement'
+// import { Link } from 'react-router-dom'
 import { useDetectOutsideClick } from "../Navbar/useDetectOutsideClick";
 import logoClean from "../Navbar/logo-clean.png";
-import { FaAngleDown } from "react-icons/fa";
+import { FaAngleDown, FaAngleRight } from "react-icons/fa";
 import "./Sidebar.css"
 
 const Sidebar = ({isOpen, toggle}) => {
@@ -18,45 +18,36 @@ const Sidebar = ({isOpen, toggle}) => {
             </Icon>
 
                 
-
-            <SidebarWrapper>   
+ 
                 <SidebarMenu>
-                
+                  
                     <div style={{marginTop: "60px"}}> 
-                    <SidebarLink to="/food-blog">
+                    <SidebarLink onClick={toggle} to="/food-blog">
                     <img src={logoClean} activeStyle style={{height: "90px", width: "90px"}} />
                     </SidebarLink>
                     <span style={{color:"#8c0608", fontSize: '30px'}} class='textLogo'>Food Inspiration</span>
                     </div>
-                 
-
-                    <div className="container">
-                          <div className="menudd-container">
-                            <button onClick={onClick} className="menudd-trigger">
-                              <span style={{fontSize: '150%', color: '#700000'}}>Рецепти&nbsp; <FaAngleDown /></span> 
+                          <div className="menusb-cont">
+                            <button onClick={onClick} className='dropbtn'>
+                              <span style={{fontSize: '150%', color: 'black'}}>Рецепти&nbsp; <FaAngleDown /></span> 
                             </button>
-          
-                            <nav ref={dropdownRef} className={`menudd ${sidebarDropIsActive ? "active" : "inactive"}`} >
-                              <ul>
-                                <li><Link onClick={toggle} to="/food-blog/recipes/salad" >Салати</Link></li>
-                                <li><Link onClick={toggle} to="/food-blog/recipes/soup" >Супи</Link></li>
-                                <li><Link onClick={toggle} to="/food-blog/recipes/appetizer" >Предястия</Link></li>
-                                <li><Link onClick={toggle} to="/food-blog/recipes/main-dish" >Основни ястия</Link></li>
-                                <li><Link onClick={toggle} to="/food-blog/recipes/dessert" >Десерти</Link></li>
-                                <li><Link onClick={toggle} to="/food-blog/recipes/pastry" >Тестени изделия</Link></li>
-                              </ul>
-                            </nav>
                           </div>
-                      </div>
-
-
-                    {/* <SidebarLink to="/food-blog/recipes"><div>Рецепти&nbsp;<FaAngleDown /></div></SidebarLink> */}
-                    <SidebarLink to="/food-blog/inspire">Вдъхнови се тук</SidebarLink>
-                    <SidebarLink to="/food-blog/about">За мен</SidebarLink>
-                    <SidebarLink to="/food-blog/contacts">Контакт</SidebarLink>
+            
+                            <div ref={dropdownRef} className={`menusb ${sidebarDropIsActive ? "active" : "inactive"}`}> 
+                            
+                            <SidebarLink onClick={toggle} to="/food-blog/recipes/salad" ><FaAngleRight />Салати</SidebarLink>
+                            <SidebarLink onClick={toggle} to="/food-blog/recipes/soup" ><FaAngleRight />Супи</SidebarLink>
+                            <SidebarLink onClick={toggle} to="/food-blog/recipes/appetizer" ><FaAngleRight />Предястия</SidebarLink>
+                            <SidebarLink onClick={toggle} to="/food-blog/recipes/main-dish" ><FaAngleRight />Основни ястия</SidebarLink>
+                            <SidebarLink onClick={toggle} to="/food-blog/recipes/dessert" ><FaAngleRight />Десерти</SidebarLink>
+                            <SidebarLink onClick={toggle} to="/food-blog/recipes/pastry" ><FaAngleRight />Тестени изделия</SidebarLink>
+                            </div>
+                    <SidebarLink onClick={toggle} to="/food-blog/inspire">Вдъхнови се тук</SidebarLink>
+                    <SidebarLink onClick={toggle} to="/food-blog/about">За мен</SidebarLink>
+                    <SidebarLink onClick={toggle} to="/food-blog/contacts">Контакт</SidebarLink>
 
                 </SidebarMenu>
-            </SidebarWrapper>
+         
         </SidebarContainer>
     )
 }
